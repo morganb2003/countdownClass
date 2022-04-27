a8b72f614ab428db9d999db6050e65a97580148e
morganb2003 <100853786+morganb2003@users.noreply.github.com>
2022-04-27 10:51:07 -0400

    fixed

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-# Name: [Your Name]
+# Name: Blaine Morgan
 # Date: 4/24/2022
-# Project: Countdown Timer using Github
-# File: countdownClass --> main.py
-#-----------------------------------------------------------------
+# Project: Countdown Timer using GitHub
+
+
+import time
+
+usertime = input('Please enter the time in seconds: ')
+
+def timercountdown(usertime):
+    while usertime:
+        min, secs = divmod(usertime, 60)
+
+        timer = '{:02d} : {:02d}' .format(min, secs)
+
+        print('\r', timer, end='')
+
+        time.sleep(1)
+
+        usertime -= 1
+
+        print('\n Timer completed')
+
+timercountdown(int(usertime))
